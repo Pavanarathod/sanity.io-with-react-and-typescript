75b9e5f09ed2ba7cc34dd84dfb4bfc89557dd705
Pavanarathod <edrocullen13@gmail.com>
2022-09-19 16:11:53 +0530

    sanity setup

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "Posted BY",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
